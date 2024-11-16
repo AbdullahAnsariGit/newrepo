dbf57abd5007993682099a04242efbd595bba0e6
Meer Quais <meerquais017@gmail.com>
2024-11-16 17:03:13 +0500

    class 45 - Constructor function

--- class-45/index.js
+++ class-45/index.js
@@ -68,14 +68,41 @@
 //   age: 18,
 // };
 
-function Students(name, rollNo, age) {
+// function Students(name, rollNo, age) {
+//   this.name = name;
+//   this.rollNo = rollNo;
+//   this.age = age;
+// }
+
+// var myStd = new Students("bilal", 123456, 20);
+// var myStd2 = new Students("affan", 123456, 18);
+
+// console.log(myStd);
+// console.log(myStd2);
+
+function Plan(name, price, space, transfer, pages, discountMonths) {
   this.name = name;
-  this.rollNo = rollNo;
-  this.age = age;
+  this.price = price;
+  this.space = space;
+  this.transfer = transfer;
+  this.pages = pages;
+  this.discountMonths = discountMonths;
+  this.calcAnnual = function (percentIfDisc) {
+    var bestPrice = this.price;
+
+    var currDate = new Date();
+    var thisMo = currDate.getMonth();
+    for (var i = 0; i < this.discountMonths.length; i++) {
+      if (this.discountMonths[i] === thisMo) {
+        bestPrice = this.price * percentIfDisc;
+        break;
+      }
+    }
+    return bestPrice * 12;
+  };
 }
 
-var myStd = new Students("bilal", 123456, 20);
-var myStd2 = new Students("affan", 123456, 18);
-
-console.log(myStd);
-console.log(myStd2);
+var p1 = new Plan("Basic", 3.99, 100, 1000, 10, [6, 7]);
+var p2 = new Plan("Premium", 5.99, 500, 5000, 50, [6, 7, 11]);
+var p3 = new Plan("Ultimate", 9.99, 2000, 20000, 500, [6, 7]);
+var annualPrice = p2.calAnnual(0.85);
