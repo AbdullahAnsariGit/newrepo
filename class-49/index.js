cc2db4a3429c432da54d79884764178fe64c7735
Meer Quais <meerquais017@gmail.com>
2024-11-30 16:27:18 +0500

    Class 49

---
+++ class-49/index.js
@@ -0,0 +1,89 @@
+var a = 10;
+function f() {
+  var b = 20;
+  console.log(a, b);
+}
+f();
+console.log(a);
+
+function f() {
+  var a = 10;
+  console.log(a);
+}
+f();
+
+console.log(a);
+
+var a = 10;
+
+// User can re-declare
+// variable using var
+var a = 8;
+
+// User can update var variable
+a = 7;
+console.log(a);
+
+console.log(a);
+var a = 10;
+
+//Let
+let a = 10;
+function f() {
+  let b = 9;
+  console.log(b);
+  console.log(a);
+}
+f();
+
+let a = 10;
+function f() {
+  if (true) {
+    let b = 9;
+
+    console.log(b);
+  }
+
+  console.log(b);
+}
+f();
+
+// It prints 10
+console.log(a);
+
+let a = 10;
+
+// It is not allowed
+let a = 10;
+
+// It is allowed
+a = 10;
+
+let a = 10;
+if (true) {
+  let a = 9;
+  console.log(a); // It prints 9
+}
+console.log(a); // It prints 10
+
+console.log(a);
+let a = 10;
+
+// Const
+const a = {
+  prop1: 10,
+  prop2: 9,
+};
+
+// It is allowed
+a.prop1 = 3;
+
+// It is not allowed
+a = {
+  b: 10,
+  prop2: 9,
+};
+
+console.log(a);
+
+var a = "alex";
