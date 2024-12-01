04959d06b2f7e716d54dab19ee98f063c0c6cbb8
abdullahansarigit <abdullahbusiness11@gmail.com>
2024-12-01 16:46:32 +0500

    49

--- class-49/index.js
+++ class-49/index.js
@@ -1,95 +1,135 @@
-var a = 10;
-function f() {
-  var b = 20;
-  console.log(a, b);
-}
-f();
-console.log(a);
+// var a = 10;
+// function f() {
+//   var b = 20;
+//   console.log(a, b);
+// }
+// f();
 
-function f() {
-  var a = 10;
-  console.log(a);
-}
-f();
+// console.log(a);
 
-console.log(a);
+// function f() {
+//   var a = 10;
+//   console.log(a);
+// }
+// f();
 
-var a = 10;
+// console.log(a);
 
-// User can re-declare
-// variable using var
-var a = 8;
+// var a = 10;
 
-// User can update var variable
-a = 7;
-console.log(a);
+// console.log(a);
+// console.log(a);
 
-console.log(a);
-var a = 10;
+// var a = 8;
+// console.log(a);
+// console.log(a);
 
-//Let
-let a = 10;
-function f() {
-  let b = 9;
-  console.log(b);
-  console.log(a);
-}
-f();
+// a = 7;
+// console.log(a);
 
-let a = 10;
-function f() {
-  if (true) {
-    let b = 9;
+// console.log(a);
+// var a = 10;
+// console.log(a);
 
-    console.log(b);
-  }
+// //Let
+// let a = 10;
 
-  console.log(b);
-}
-f();
+// function f() {
+//   let b = 9;
+//   console.log(a);
+// }
+// // f();
+// console.log(b);
 
-// It prints 10
-console.log(a);
+// let a = 10;
+// function f() {
+//   if (true) {
+//     let b = 9;
 
-let a = 10;
+//     console.log(b);
+//   }
 
-// It is not allowed
-let a = 10;
+//   console.log(b);
+// }
+// f();
 
-// It is allowed
-a = 10;
+// console.log(a);
 
-let a = 10;
-if (true) {
-  let a = 9;
-  console.log(a); // It prints 9
-}
-console.log(a); // It prints 10
+// let a = 10;
 
-console.log(a);
-let a = 10;
+// let a = 10;
 
-// Const
-const a = {
-  prop1: 10,
-  prop2: 9,
-};
+// a = 10;
 
-// It is allowed
-a.prop1 = 3;
+// let a = 10;
+// if (true) {
+//   let a = 9;
+//   console.log(a)
+// }
+// console.log(a)
 
-// It is not allowed
-a = {
-  b: 10,
-  prop2: 9,
-};
+// console.log(a);
+// let a = 10;
 
-console.log(a);
+// // Const
+// const a = {
+//   prop1: 10,
+//   prop2: 9,
+// };
 
-var a = "alex";
+// a.prop1 = 3;
+
+// a = {
+//   b: 10,
+//   prop2: 9,
+// };
+
+// console.log(a);
+
+// var a = "alex";
 
 // Helpful Resources:
 
 // https://www.geeksforgeeks.org/difference-between-var-let-and-const-keywords-in-javascript/
 // https://blog.coolhead.in/difference-between-function-scope-and-block-scope-in-javascript
 // https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/
+
+// var a = 3;
+// const a = 3;
+// let b = 6;
+
+// console.log(a);
+// f();
+
+// function f() {
+//   let b = 8;
+//   console.log(b);
+//   console.log(a);
+//   if (a) {
+//     console.log(a);
+//   } else {
+//     console.log(b);
+//   }
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// Hadi
+// type e
+// 8
+//not defined
+
+//bilal & ali
+// 3
+// 8
+// 3
+// 3
+// 3
+// 3
+
+//Sameer
+//type err
+//8
+//4
+//err
